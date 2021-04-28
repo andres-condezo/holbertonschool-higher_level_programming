e3fc2c5fc9437d8abf5eb6d7b3fc7300c137ce98
adrs <ra.condezomonge@gmail.com>
2021-04-28 04:32:09 -0500

    Feat(12-fizzbuzz.py): Create 12-fizzbuzz.py script

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+
+    for num in range(1, 101):
+        if ((num % 3) == 0 and (num % 5) == 0):
+            print("{}".format("FizzBuzz "), end='')
+        elif ((num % 3) == 0):
+            print("{}".format("Fizz "), end='')
+        elif ((num % 5) == 0):
+            print("{}".format("Buzz "), end='')
+        else:
+            print("{:d} ".format(num), end='')
