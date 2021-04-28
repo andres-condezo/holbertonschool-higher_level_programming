a94ab3e006c4380f1ecd17e90b6b4bbcb15327ae
adrs <ra.condezomonge@gmail.com>
2021-04-28 03:21:40 -0500

    Feat(8-uppercase): Create 8-uppercase script

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,7 @@
 def uppercase(str):
     for c in str:
         if (ord(c) >= 97 and ord(c) <= 122):
-            print("{}".format(chr(ord(c) - 32)), end='')
-        else:
-            print("{}".format(c), end='')
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end='')
 
     print()
