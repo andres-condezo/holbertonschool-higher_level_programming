f06db494a3e4ef9f257fd354cef4d30b1db820d4
adrs <ra.condezomonge@gmail.com>
2021-04-28 05:21:57 -0500

    Fix(101-remove_char_at.py): Add validations to  101-remove_char_at.py script

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if len(str) > n:
+    if (n < 0):
+        return str
+
+    if (len(str) > n):
         str = str[0: n] + str[n + 1:]
 
     return str
