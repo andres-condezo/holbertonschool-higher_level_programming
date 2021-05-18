368af055daaad8eb52edf1b9fa166b904888d092
adrs condezo <ra.condezomonge@gmail.com>
2021-05-18 09:28:11 -0500

    Feat(1-calculation): [Create 1-calculation program]

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import calculator_1 as m
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print('{:d} + {:d} = {:d}'.format(a, b, m.add(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, m.sub(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, m.mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, m.div(a, b)))
