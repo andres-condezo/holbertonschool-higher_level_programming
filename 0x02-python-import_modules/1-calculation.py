26fba9f3b6d7ed1a20b8a9be4cbf35fffef7f5ee
adrs condezo <ra.condezomonge@gmail.com>
2021-05-18 09:57:34 -0500

    Fix(1-calculation.py): [Change impot line]

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import calculator_1 as m
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
-    print('{:d} + {:d} = {:d}'.format(a, b, m.add(a, b)))
-    print('{:d} - {:d} = {:d}'.format(a, b, m.sub(a, b)))
-    print('{:d} * {:d} = {:d}'.format(a, b, m.mul(a, b)))
-    print('{:f} / {:f} = {:f}'.format(a, b, m.div(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
