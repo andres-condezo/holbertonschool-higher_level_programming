75b60564b15d279b065cebba4d7062921832c49a
adrs condezo <ra.condezomonge@gmail.com>
2021-05-18 09:32:59 -0500

    Fix(1-calculation.py): [Change a variable type from integer to float]

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     print('{:d} + {:d} = {:d}'.format(a, b, m.add(a, b)))
     print('{:d} - {:d} = {:d}'.format(a, b, m.sub(a, b)))
     print('{:d} * {:d} = {:d}'.format(a, b, m.mul(a, b)))
-    print('{:d} / {:d} = {:d}'.format(a, b, m.div(a, b)))
+    print('{:f} / {:f} = {:f}'.format(a, b, m.div(a, b)))
