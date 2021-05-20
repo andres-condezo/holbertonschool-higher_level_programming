a6b78d3cde7be4018afa85453d8debdfb3f7aba7
adrs condezo <ra.condezomonge@gmail.com>
2021-05-20 11:52:35 -0500

    Feat(0-square_matrix_simple.py): [Add 0-square_matrix_simple function]

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+
+    new_matrix = []
+    for row in matrix:
+        new_matrix.append(list(map(lambda x: x * x, row)))
+    return new_matrix
