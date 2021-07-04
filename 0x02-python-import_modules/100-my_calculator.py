9d95855af38bcbba5ef806deab40937b7f20db88
adrs condezo <ra.condezomonge@gmail.com>
2021-07-04 15:30:52 -0500

    Add(100.my_calculator.py): Add my_calculator.py function

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-"""my_calculator module"""
 
 from sys import argv
 from calculator_1 import add, sub, mul, div
@@ -7,7 +6,6 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
 
-    """Main function"""
     dic_op = {"+": add, "-": sub, "*": mul, "/": div}
 
     if len(argv) < 4:
