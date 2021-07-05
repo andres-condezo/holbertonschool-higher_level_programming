c5fe415e6c5d6d066a511c0bb3998c5ce0ed0d09
adrs condezo <ra.condezomonge@gmail.com>
2021-07-05 02:17:41 -0500

    Feat(3-rectangle.py): Add 3-rectangle function

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Reactangle module"""
+
+
+class Rectangle:
+    """A class to create rectangle objects"""
+
+    def __init__(self, width=0, height=0):
+        """Constructor"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if (self.height == 0 or self.width == 0):
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Print the rectangle with the character #"""
+        if (self.height == 0 or self.width == 0):
+            return ""
+        image = (("#" * self.width) + '\n') * self.height
+        return image
