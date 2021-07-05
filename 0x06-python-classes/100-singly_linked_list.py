a826bb270f89b18e3243b8acafbf40694bce903c
adrs condezo <ra.condezomonge@gmail.com>
2021-07-04 21:30:58 -0500

    Feat(100-singly_linked_list.py): Add docstrings to singly_linked_list function

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,32 +1,41 @@
 #!/usr/bin/python3
+"""singly linked list module"""
+
 
 class Node:
+    """Node class"""
+
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """get __data attribute"""
         return self.__data
 
     @data.setter
     def data(self, value):
+        """set __data attribute"""
         if type(value) is not int:
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """get __next_node attribute"""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """set __next_node attribute"""
         if type(value) != Node and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """SiglyLinkedList class"""
 
     def __init__(self):
         self.__head = None
@@ -40,6 +49,8 @@ class SinglyLinkedList:
         return "\n".join(list_nodes)
 
     def sorted_insert(self, value):
+        """Public instance method that inserts a new Node into the
+           correct sorted position in the list (increasing order)"""
         new_node = Node(value)
 
         if self.__head is None:
