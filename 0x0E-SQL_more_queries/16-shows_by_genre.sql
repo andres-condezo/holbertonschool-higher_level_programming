8a312e43c9de23b051cba0866e09523b28db98f0
adrs condezo <ra.condezomonge@gmail.com>
2021-07-14 04:05:24 -0500

    Feat(16-shows_by_genre.sql):[Add shows_by_genre]

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
+ON tv_show_genres.show_id = tv_shows.id
 LEFT JOIN tv_genres
-ON tv_show_genres.gender_id = tv_genres.id
-ORDER BY tv_shows.title, tv_genres.name
+ON tv_genres.id = tv_show_genres.gender_id
+ORDER BY tv_shows.title, tv_genres.name;
