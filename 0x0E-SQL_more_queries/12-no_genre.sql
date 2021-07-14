8b34277ee4a73e141d7ffdf78ac7e0f7e42a0536
adrs condezo <ra.condezomonge@gmail.com>
2021-07-13 23:18:01 -0500

    Feat(12-no_genre.sql):[Add no_genre]

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,5 +3,5 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows_genres
 LEFT JOIN tv_show
 ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show.id IS NULL
+WHERE tv_show_genres.id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
