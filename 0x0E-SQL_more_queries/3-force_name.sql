fd8fbd2ebe2ff1d6ec9b4c36a40e80fdd1e11e38
adrs condezo <ra.condezomonge@gmail.com>
2021-07-13 19:32:43 -0500

    Feat(3-force_name.sql):[Add 3-force_name]

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 --  a script that creates the table force_name on your MySQL server
-CREATE TABLE IF NOT EXITST force_name(id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXIST force_name (id INT, name VARCHAR(256) NOT NULL);
