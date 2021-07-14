aed0588b4a085f61fb5a861338ad168cc045b857
adrs condezo <ra.condezomonge@gmail.com>
2021-07-13 21:08:06 -0500

    Feat(7-cities.sql):[add cities file]

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- a script that creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
-(id INT UNIQUE AUTO_INCREMENT NOT NULL,
+(id INT AUTO_INCREMENT,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
 PRIMARY KEY (id),
