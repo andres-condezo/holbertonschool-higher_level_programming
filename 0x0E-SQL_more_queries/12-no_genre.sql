d056b92fe52bc8bfb2129f27634912704ffed366
adrs condezo <ra.condezomonge@gmail.com>
2021-07-13 23:21:34 -0500

    Feat(12-no_genre.sql):[Add no_genre]

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- a script that lists all shows contained in the database
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows_genres
+FROM tv_show_genres
 RIGHT JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_show_genres.show_id IS NULL
