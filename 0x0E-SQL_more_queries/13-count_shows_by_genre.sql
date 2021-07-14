2d1cadd4dc5094dc3959bb9665cb36f1c8060c05
adrs condezo <ra.condezomonge@gmail.com>
2021-07-13 23:49:06 -0500

    Feat(13-count_shows_by_genre.sql):[Add count_shows_by_genre script]

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
-SELECT tv_genres.name AS genre, COUNT(tv_show_generes.show_id) AS number_of_shows
+SELECT tv_genres.name AS "genre", count(tv_show_generes.show_id) AS "number_of_shows"
 FROM tv_show_genres
 INNER JOIN tv_genres
 ON tv_show_genres.gender_id = tv_genres.id
