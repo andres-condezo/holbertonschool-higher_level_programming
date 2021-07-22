dd69868313040ab3cdbaf57640042de6c086c025
adrs condezo <ra.condezomonge@gmail.com>
2021-07-22 13:53:31 -0500

    Feat(4-hidden_discovery.py):[Add hidden_discovery script]

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    a = dir(hidden_4)
-    for i in range(len(a)):
-        if i[a][:2] != "__":
-            print(a[i])
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
