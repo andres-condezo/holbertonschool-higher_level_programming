db83b60bc45ea41de4ebc3e498ca11713933ac0b
adrs condezo <ra.condezomonge@gmail.com>
2021-07-22 14:04:14 -0500

    Feat(102-magic_calculation.py):[Add magic calculation script]

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,8 @@
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
+        c = add(c, i)
         for i in range(4, 6):
-            c = add(c, i)
-        return c
-    return sub(a, b)
+        return c = add(c, i)
+    else:
+        return sub(a, b)
