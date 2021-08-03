3df0a605f094624596db23d3347ea4b04e9e4970
adrs condezo <ra.condezomonge@gmail.com>
2021-08-02 23:15:39 -0500

    Style(2-arguments.js):[Fix style on arguments script]

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // a script that prints a message depending of the number of arguments passed
 
-let args = process.argv;
+const args = process.argv;
 
 if (args.length === 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (args.length === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
