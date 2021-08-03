bf4af2b9c270953e733924c9222245ccd5ea6734
adrs condezo <ra.condezomonge@gmail.com>
2021-08-03 02:23:28 -0500

    Style(9-add.js):[Add add script]

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// a script that prints the addition of 2 integers
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+function add (a, b) {
+  console.log(a + b);
+}
+
+add(num1, num2);
