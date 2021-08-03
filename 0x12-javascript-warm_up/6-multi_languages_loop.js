53165fb3e54cdccea3a75d85e17884214ab3cabd
adrs condezo <ra.condezomonge@gmail.com>
2021-08-03 00:03:20 -0500

    Style(6-multi_languages_loop.js):[add multi_languages_loop script]

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// a script that prints 3 lines
+
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
