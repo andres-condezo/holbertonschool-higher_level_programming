a975c9169efae3659f7605f07f484945b6e5c147
adrs condezo <ra.condezomonge@gmail.com>
2021-08-04 02:00:51 -0500

    Feat(7-rectanble.js):[Add 7-rectanble script]

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a function that returns the number of occurrences in a list
+
+exports.nbOccurences = function (list, searchElement) {
+  occurrences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      occurrences += 1;
+    }
+  }
+  return occurrences;
+};
