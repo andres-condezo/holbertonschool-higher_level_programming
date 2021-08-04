1b137e647bac2a608b34b8a70d6aafb0e0e100ca
adrs condezo <ra.condezomonge@gmail.com>
2021-08-03 23:36:11 -0500

    Feat(1-rectanble.js):[Add 1-rectanble script]

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// an empty class Rectangle that defines a rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
