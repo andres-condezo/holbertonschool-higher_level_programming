9a078f90968ae17a4595a70c232133d2647d902b
andres <ra.condezomonge@gmail.com>
2021-08-07 19:45:24 -0500

    Feat(0-select_states.py):[Add select_states script]

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""a script that lists all states from the database hbtn_0e_0_usa"""
+
+def main():
+    USER = argv[1]
+    PASS = argv[2]
+    DATABASE = argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=USER, passwd=PASS, database=DATABASE)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id")
+
+    query = cursor.fetchall()
+
+    for state in query:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    main()
