fbfbcf6c3a6eeaf04bc516352a005d501b080a39
andres <adrsp@pop-os.localdomain>
2021-08-08 21:38:22 -0500

    Feat(1-filter_states.py):[Add filter_states script]

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""a module for a script that list all states starting with N"""
+
+
+def main():
+    """lists all states starting with N from database hbtn_0e_0_usa"""
+
+    USER = argv[1]
+    PASS = argv[2]
+    DATABASE = argv[3]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=USER,
+                         passwd=PASS,
+                         database=DATABASE)
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE SUBSTRING(name,1,1) = 'N' ORDER BY id")
+    query = cursor.fetchall()
+
+    for state in query:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    main()
