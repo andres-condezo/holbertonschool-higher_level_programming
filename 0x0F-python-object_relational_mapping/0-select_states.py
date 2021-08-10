7de1593d86920070f27daed5c6a4a7e929d6534d
Andres Condezo <ra.condezo@gmail.com>
2021-08-09 19:51:34 -0500

    Feat(0-select_states.py):[Add query function]

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,7 @@
 """a module for a script that list all states"""
 
 
-def main():
+def query():
     """a script that lists all states from the database hbtn_0e_0_usa"""
 
     USER = argv[1]
@@ -15,14 +15,24 @@ def main():
                          passwd=PASS,
                          database=DATABASE)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id")
+    cursor.execute("SELECT id, name FROM states ORDER BY id")
     query = cursor.fetchall()
 
+    my_list = []
     for state in query:
-        print(state)
+        my_list.append(state)
 
     cursor.close()
     db.close()
+
+    return my_list
+
+
+def main():
+    my_list = query()
+    if my_list is not None:
+        for state in my_list:
+            print(state)
 
 
 if __name__ == "__main__":
