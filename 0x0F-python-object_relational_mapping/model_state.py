1d7cadae8468b4d923ffbca100af4971a84f2497
andres-condezo <ra.condezomonge@gmail.com>
2021-08-13 15:11:44 -0500

    Feat(model_state.py): [Add model_state]

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,8 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """Definition of a stat class"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
